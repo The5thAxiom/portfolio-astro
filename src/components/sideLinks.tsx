6ba24d30687c99316a03346f251d3e0875a6b2eb
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-16 15:14:33 +0530

    refactoring

--- src/components/sideLinks.tsx
+++ src/components/sideLinks.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import OutboundLink from './outboundLink';
 import './sidelinks.css';
-import {
-    GithubIcon,
-    BlogIcon,
-    LinkedinIcon,
-    InstagramIcon
-} from '../components/icons';
+import { GithubIcon, BlogIcon, LinkedinIcon, InstagramIcon } from './icons';
 
 type socialLink = { name: string; link: string; icon?: any };
 export const socialLinks: socialLink[] = [
